f869f88fc508c26428567fdfbd0e14b91d0a9156
faraujo <faraujo@codata.pb.gov.br>
2014-12-17 11:30:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f2ebd814a359555df1c2905b33f7e0e9e965d52529fc43ccfefb6caec98cd83db8cd5804a0b0ab3bf398a7f75ad97840304d211d512ec7b8bce23dbf062160e1'
